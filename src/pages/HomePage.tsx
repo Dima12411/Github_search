e674655f92e5bcf6afee20cbb95cc81a4e8d2033
Dima12411 <fixer40delta@gmail.com>
2022-07-17 22:53:26 +0300

    getting user repositories by click

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {useSearchUsersQuery} from "../store/github/github.api";
+import {useLazyGetUserReposQuery, useSearchUsersQuery} from "../store/github/github.api";
 import {useDebounce} from "../hooks/debounce";
 
 const HomePage = () => {
@@ -10,10 +10,15 @@ const HomePage = () => {
         skip: debounced.length < 3,
         refetchOnFocus: true
     })
+    const [fetchRepos, {isLoading: areReposLoading, data: repos}] = useLazyGetUserReposQuery()
 
     useEffect(() => {
         setDropdown(debounced.length > 3 && data?.length! > 0)
     }, [debounced, data])
+
+    const clickHandler = (userName: string) => {
+        fetchRepos(userName)
+    }
 
     return (
         <div className='flex justify-center pt-10 mx-auto h-screen w-screen'>
@@ -27,15 +32,22 @@ const HomePage = () => {
                        onChange={e => setSearch(e.currentTarget.value)}
                 />
 
-                {dropdown && <ul className='list-none absolute top-[42px] left-0 right-0 max-h-[200px] overflow-y-scroll shadow-md bg-white'>
-                    { isLoading && <p className='text-center'>Loading...</p> }
-                    { data?.map(user => (
-                        <li key={user.id}
-                        className='py-2 px-4 hover:bg-gray-500 hover:text-white transition-colors cursor-pointer'
-                        >{user.login}
-                        </li>
-                    )) }
-                </ul>}
+                {dropdown &&
+                    <ul className='list-none absolute top-[42px] left-0 right-0 max-h-[200px] overflow-y-scroll shadow-md bg-white'>
+                        {isLoading && <p className='text-center'>Loading...</p>}
+                        {data?.map(user => (
+                            <li
+                                key={user.id}
+                                onClick={() => clickHandler(user.login)}
+                                className='py-2 px-4 hover:bg-gray-500 hover:text-white transition-colors cursor-pointer'
+                            >{user.login}
+                            </li>
+                        ))}
+                    </ul>}
+                <div className="container">
+                    {areReposLoading && <p className='text-center'>Repos are loading...</p>}
+                    { repos?.map(repo => <p>{repo.url}</p>) }
+                </div>
             </div>
         </div>
     );
