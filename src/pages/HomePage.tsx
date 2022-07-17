d89117e77fb4424b794636c622f1e07be186a46f
Dima12411 <fixer40delta@gmail.com>
2022-07-17 16:34:16 +0300

    getting users by the specified parameter

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import {useSearchUsersQuery} from "../store/github/github.api";
 
 const HomePage = () => {
+    const {isLoading, isError, data} = useSearchUsersQuery("dima")
     return (
         <div>
             home
